86beb24e47648774c285e0eef390c7fc3600aa04
Chi Hoang <Chi_Hoang@intuit.com>
2019-10-15 16:08:53 -0700

    Initial project setup

---
+++ src/test/java/com/storage/app/StorageManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.storage.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StorageManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
