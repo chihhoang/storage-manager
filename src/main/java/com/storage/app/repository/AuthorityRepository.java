a0173fa6ebc14473224c691a2364d21d1b78a0d3
Chi Hoang <Chi_Hoang@intuit.com>
2019-10-16 20:17:25 -0700

    Add repos

---
+++ src/main/java/com/storage/app/repository/AuthorityRepository.java
@@ -0,0 +1,8 @@
+package com.storage.app.repository;
+
+import com.storage.app.model.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/storage/app/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.storage.app.repository;
+
+import com.storage.app.model.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findOneByEmailIgnoreCase(String email);
+
+  Optional<User> findOneByLogin(String login);
+}
