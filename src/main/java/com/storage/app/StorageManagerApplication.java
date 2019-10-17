6cbaff9a531478a7ee5b03b085141b307ba81db1
Chi Hoang <Chi_Hoang@intuit.com>
2019-10-16 20:14:09 -0700

    Exclude security auto config

--- src/main/java/com/storage/app/StorageManagerApplication.java
+++ src/main/java/com/storage/app/StorageManagerApplication.java
@@ -1,23 +1,20 @@
 package com.storage.app;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
 @Slf4j
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class StorageManagerApplication {
   @Value("${app.env}")
   private String env;
-
-  @Autowired private Environment environment;
 
   public static void main(String[] args) {
     SpringApplication.run(StorageManagerApplication.class, args);
