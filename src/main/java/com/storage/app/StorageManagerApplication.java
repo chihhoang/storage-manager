fb17a59b12b695f155f5058f9099b11fd22cf914
Chi Hoang <Chi_Hoang@intuit.com>
2019-10-23 19:32:12 -0700

    Create admin on startup

--- src/main/java/com/storage/app/StorageManagerApplication.java
+++ src/main/java/com/storage/app/StorageManagerApplication.java
@@ -1,5 +1,10 @@
 package com.storage.app;
 
+import com.google.common.collect.Sets;
+import com.storage.app.model.Role;
+import com.storage.app.model.UserDTO;
+import com.storage.app.service.UserService;
+import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -16,6 +21,8 @@ public class StorageManagerApplication {
   @Value("${app.env}")
   private String env;
 
+  @Resource private UserService userService;
+
   public static void main(String[] args) {
     SpringApplication.run(StorageManagerApplication.class, args);
   }
@@ -24,6 +31,21 @@ public class StorageManagerApplication {
   public CommandLineRunner startup() {
     return args -> {
       log.info("========== Starting app... in {} env", env);
+
+      UserDTO admin =
+          UserDTO.builder()
+              .login("admin")
+              .password("admin")
+              .email("chhoang102@gmail.com")
+              .activated(true)
+              .createdBy(Role.ROLE_ADMIN.value())
+              .firstName("Chi")
+              .lastName("Hoang")
+              .imageUrl("http://example.com")
+              .roles(Sets.newHashSet(Role.ROLE_ADMIN, Role.ROLE_USER))
+              .build();
+
+      userService.createUser(admin);
     };
   }
 }
