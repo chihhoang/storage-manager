8133dbc72746be5ff9a57e416d6f8018d1469039
Chi Hoang <chhoang102@gmail.com>
2019-10-27 12:33:04 -0700

    Change env to prod

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 kill -9 $(lsof -i:9000 -t) 2> /dev/null
-./mvnw spring-boot:run -Dspring-boot.run.arguments=--spring.profiles.active=local
+./mvnw spring-boot:run -Dspring-boot.run.arguments=--spring.profiles.active=prod
