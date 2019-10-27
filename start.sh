55de7bd5e763e1818c7f2a36ceeb41d93dbb48ee
Chi Hoang <chhoang102@gmail.com>
2019-10-27 12:43:18 -0700

    Redirect logs

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 kill -9 $(lsof -i:9000 -t) 2> /dev/null
-./mvnw spring-boot:run -Dspring-boot.run.arguments=--spring.profiles.active=prod
+./mvnw spring-boot:run -Dspring-boot.run.arguments=--spring.profiles.active=prod > ~/storagepro.log
