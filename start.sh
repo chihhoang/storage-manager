6068c9149e6261fd83dfad3a1a4046a18046a506
Chi Hoang <chhoang102@gmail.com>
2019-11-08 18:55:59 -0800

    more echo

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
-echo DATE_WITH_TIME
+echo $DATE_WITH_TIME
 
 kill -9 $(lsof -i:9000 -t) 2> /dev/null
+echo "Killed port 9000"
 nohup ./mvnw spring-boot:run -Dspring-boot.run.arguments=--spring.profiles.active=prod > ~/storagemanager-${DATE_WITH_TIME}.log &
+
+echo "Started the application"
