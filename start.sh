9a03a5b5ae3a36e6e4ad08853a4603db68ef0369
Chi Hoang <chhoang102@gmail.com>
2019-11-08 18:53:22 -0800

    echo log time

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
+echo DATE_WITH_TIME
 
 kill -9 $(lsof -i:9000 -t) 2> /dev/null
-nohup ./mvnw spring-boot:run -Dspring-boot.run.arguments=--spring.profiles.active=prod > ~/storagepro-${DATE_WITH_TIME}.log &
+nohup ./mvnw spring-boot:run -Dspring-boot.run.arguments=--spring.profiles.active=prod > ~/storagemanager-${DATE_WITH_TIME}.log &
